7b6c7664050e18aa42ac255fd9902e3435d56a5a
Gerardo Cervantes <gcervantes8@miners.utep.edu>
2016-11-20 09:03:33 -0700

    Added switch function, clicking on button makes it go reverse

--- p2swLib/p2switches.c
+++ p2swLib/p2switches.c
@@ -1,3 +1,4 @@
+
 #include <msp430.h>
 #include "p2switches.h"
 
@@ -9,9 +10,9 @@ static void
 switch_update_interrupt_sense()
 {
   switches_current = P2IN & switch_mask;
-  /* update switch interrupt to detect changes from current buttons */
-  P2IES |= (switches_current);  /* if switch up, sense down */
-  P2IES &= (switches_current | ~switch_mask); /* if switch down, sense up */
+  //* update switch interrupt to detect changes from current buttons 
+  P2IES |= (switches_current);  // if switch up, sense down 
+  P2IES &= (switches_current | ~switch_mask); // if switch down, sense up 
 }
 
 void 
@@ -19,7 +20,7 @@ p2sw_init(unsigned char mask)
 {
   switch_mask = mask;
   P2REN |= mask;    /* enables resistors for switches */
-  P2IE = mask;      /* enable interrupts from switches */
+  P2IE = mask;      /* enable interrupts from switches */  //P2IE |= mask
   P2OUT |= mask;    /* pull-ups for switches */
   P2DIR &= ~mask;   /* set switches' bits for input */
   P2IES |= mask;    
@@ -36,12 +37,12 @@ p2sw_read() {
   unsigned int sw_changed = switches_current ^ switches_last_reported;
   switches_last_reported = switches_current;
   return switches_current | (sw_changed << 8);
-}
+  }
 
 /* Switch on P2 (S1) */
-__interrupt(PORT2_VECTOR) Port_2(){
-  if (P2IFG & switch_mask) {  /* did a button cause this interrupt? */
-    P2IFG &= ~switch_mask;	/* clear pending sw interrupts */
+/*__interrupt(PORT2_VECTOR) Port_2(){
+  if (P2IFG & switch_mask) {  // did a button cause this interrupt? 
+  P2IFG &= ~switch_mask;	// clear pending sw interrupts 
     switch_update_interrupt_sense();
   }
-}
+  }*/
